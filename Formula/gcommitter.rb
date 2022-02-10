df2d8160144990f079512dada523c407458a10ba
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-10 17:26:17 +0800

    Brew formula update for gcommitter version v2.2.4

--- Formula/gcommitter.rb
+++ Formula/gcommitter.rb
@@ -5,20 +5,20 @@
 class Gcommitter < Formula
   desc "Easy way of git commit and push"
   homepage "https://github.com/zcong1993/gcommitter"
-  version "2.2.3"
+  version "2.2.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/zcong1993/gcommitter/releases/download/v2.2.3/gcommitter_2.2.3_Darwin_arm64.tar.gz"
-      sha256 "a510af81562515d888ca2f922dd9a5ff7633e8b8e8a4c2a1b71774e386a0802e"
+      url "https://github.com/zcong1993/gcommitter/releases/download/v2.2.4/gcommitter_2.2.4_Darwin_arm64.tar.gz"
+      sha256 "6eeb696a942b2110a99f25b927d035459c26bd25dde10046f0be3af9379546ed"
 
       def install
         bin.install "gcommitter"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/zcong1993/gcommitter/releases/download/v2.2.3/gcommitter_2.2.3_Darwin_x86_64.tar.gz"
-      sha256 "4678650815e89b0cd3bea9557984cc03a7b5f0c630cec7b81cfa1b7657a1dfa3"
+      url "https://github.com/zcong1993/gcommitter/releases/download/v2.2.4/gcommitter_2.2.4_Darwin_x86_64.tar.gz"
+      sha256 "412746f52553213d5f1630470008dcaf796c0a626865e2145fec4a61a7c19233"
 
       def install
         bin.install "gcommitter"
@@ -28,16 +28,16 @@ class Gcommitter < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/zcong1993/gcommitter/releases/download/v2.2.3/gcommitter_2.2.3_Linux_x86_64.tar.gz"
-      sha256 "4c8f9dd9efb7a3731e05ffe52b28c86d8fbde9317fbd890000f9937283b8041f"
+      url "https://github.com/zcong1993/gcommitter/releases/download/v2.2.4/gcommitter_2.2.4_Linux_x86_64.tar.gz"
+      sha256 "375155211958c3aaea8acfc143c979b45d6d737696f27ee6007d69032842e0df"
 
       def install
         bin.install "gcommitter"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/zcong1993/gcommitter/releases/download/v2.2.3/gcommitter_2.2.3_Linux_arm64.tar.gz"
-      sha256 "10cc4fe2b2ad0eafbeca804e9e60e451c12b69c0ed8e55b9dcbc60ffd0a50a9e"
+      url "https://github.com/zcong1993/gcommitter/releases/download/v2.2.4/gcommitter_2.2.4_Linux_arm64.tar.gz"
+      sha256 "6bbdbfef9c5e05e220121711c643a6b79fa6b44d9010e1662019a385bd862086"
 
       def install
         bin.install "gcommitter"
